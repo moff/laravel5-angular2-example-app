a930056782775c96859427af9ffbeb1442a7a393
Paul Moff <moff.dev@gmail.com>
2016-08-10 20:00:44 +0700

    bootstrap css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,10 @@
     <head>
         <title>Laravel5 + Angular2 application</title>
         <base href='/'>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
     <body>
         <main-component>Loading...</main-component>
