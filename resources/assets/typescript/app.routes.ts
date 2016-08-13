10eaa9ad76d06aa6c75cfbc9a436fcedd9f9c666
Paul Moff <moff.dev@gmail.com>
2016-08-13 13:40:56 +0700

    routing file

---
+++ resources/assets/typescript/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes, RouterModule } from '@angular/router';
+import { HeroesComponent } from './heroes.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'heroes',
+    component: HeroesComponent
+  }
+];
