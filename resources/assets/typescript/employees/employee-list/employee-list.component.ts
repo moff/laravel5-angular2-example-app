d6b16989cb950313d15911d7005da4a7a5bd2903
Paul Moff <moff.dev@gmail.com>
2016-08-29 12:45:39 +0700

    added spinner

--- resources/assets/typescript/employees/employee-list/employee-list.component.ts
+++ resources/assets/typescript/employees/employee-list/employee-list.component.ts
@@ -1,29 +1,42 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, DoCheck } from "@angular/core";
 import { EmployeeService } from '../shared/employee.service';
 import { IEmployee } from '../shared/employee.interface';
 import { Subscription } from 'rxjs/Subscription';
+import { LoadersCssComponent } from 'angular2-loaders-css';
 
 @Component({
-    template: require('./employee-list.component.html')
+    template: require('./employee-list.component.html'),
+    directives: [LoadersCssComponent]
 })
-export class EmployeeListComponent implements OnInit {
+export class EmployeeListComponent implements OnInit, DoCheck {
     
     private _sub: Subscription;
     
-    employees: IEmployee[];
+    employees: IEmployee[] = [];
     errorMessage: string;
+    loading: boolean = true;
     
     constructor(private _employeeService: EmployeeService) {}
     
     ngOnInit(): void {
         this._sub = this._employeeService.getEmployees()
             .subscribe(
-                employees => this.employees = employees,
+                employees => { 
+                    setTimeout(() => {
+                        this.employees = employees; 
+                    }, 1500);
+                },
                 error => this.errorMessage = <any>error);
     }
     
     clickMe(): void {
         console.log(this.employees);
+    }
+    
+    ngDoCheck(): void {
+        if (this.employees.length) {
+            this.loading = false;
+        }
     }
     
     ngOnDestroy(): void {
